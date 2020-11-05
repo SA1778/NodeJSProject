09cac42bac7b8257a6e7a21cf6a983974aa439e8
Thresher1436 <Iamthebadwolf80@gmail.com>
2020-11-05 11:00:55 -0500

    asodhas

--- website/chartDef.js
+++ website/chartDef.js
@@ -1,13 +1,17 @@
+//Variables
+let data = [Math.floor(Math.random()*10), Math.floor(Math.random()*10), Math.floor(Math.random()*10), Math.floor(Math.random()*10), Math.floor(Math.random()*10)];
 let ctx = document.getElementById('myChart').getContext('2d');
+
+//Create Chart
 let chartA = new Chart(ctx, {
     type: 'line',
     data: {
         labels:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
         datasets:[{
-            data:[10, 60, 40, 65, 20],
-            label:"Temperature",
-            borderColor: "#0004e4",
-            backgroundColor: "#0004e42f",
+            data: data,
+            label:"Data",
+            borderColor: "#000be4",
+            backgroundColor: "#000be44f",
             fill: true
         }]
     },
@@ -20,3 +24,10 @@ let chartA = new Chart(ctx, {
         }
     }
 });
+
+//Function Definitions
+function randomizeData(){
+    let newData = data.map(x => Math.floor(Math.random()*50));
+    chartA.data.datasets[0].data = newData;
+    chartA.update();
+}
